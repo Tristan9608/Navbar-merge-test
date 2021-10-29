c3fc66c02f03d71cf1814916760bf6bcdfa8bf0f
unknown <uklee@tangunsoft.com>
2021-10-29 09:13:27 +0900

    import icon

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import Icon from '../Icon';
+import Icon from '../Icon/Icon';
 //import { NavStyles } from './Nav.styles';
 import './Nav.css';
 //import Button from '../Button/Button';
